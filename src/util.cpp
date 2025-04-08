2ea0a4bef7b328762e6f695a2e5515b9740995bb
Valeron93 <valera1800@gmail.com>
2025-04-08 02:20:37 +0200

    macOS build: emit bundle, build Universal binary in CI

--- src/util.cpp
+++ src/util.cpp
@@ -1,11 +1,26 @@
 #include "util.h"
+#include <SDL3/SDL.h>
 
 namespace util {
 
+auto get_resources_path()
+{
+#ifdef __APPLE__
+    auto path = SDL_GetBasePath();
+#else
+    auto path = SDL_GetCurrentDirectory();
+#endif
+    return path;
+}
+
 std::string read_all_text(const char* filename)
 {
+    std::filesystem::path base = get_resources_path();
+    std::filesystem::path path = filename;
+
+    auto filepath = base / path;
     std::stringstream ss;
-    std::ifstream file(filename);
+    std::ifstream file(filepath);
     ss << file.rdbuf();
     return ss.str();
 }
